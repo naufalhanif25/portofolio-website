e450a5d6bf1898f57d2d5e081d1295ebc8a11a77
Naufal Hanif <naufal.hanif2525@gmail.com>
2025-01-15 18:23:26 +0700

    Added Laravel files

---
+++ resources/views/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Naufal Hanif</title>
+</head>
+<body>
+    <div class="title-style">
+        <h1 class="title">
+            Naufal Hanif
+        </h1>
+    </div>
+    <div class="box-style">
+        <div class="box">
+            <h2 class="about">Profile
+                <p class="about-paragraph">
+                    Halo nama saya Naufal Hanif, saya adalah seorang mahasiswa informatika dari Universitas Syiah Kuala
+                </p>
+            </h2>
+        </div>
+    </div>
+</body>
+</html>
