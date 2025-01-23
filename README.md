d0593f168dfc22c37a6816c80b07d32dda0a3e2a
Naufal Hanif <naufal.hanif2525@gmail.com>
2025-01-23 22:25:31 +0700

    Updated the user interface and added new features

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# portofolio-website
+<!-- Title -->
+<div align="center">
+    <h1>Protofolio Website</h1>
+</div>
+
+<!-- Badges -->
+<div align="center">
+    <img src="https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white" /> 
+    <img src="https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white" /> 
+    <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" /> 
+    <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" /> 
+    <img src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white" />
+</div>
+
+<!-- Descriptions -->
+<h2>
+    📝 Description
+</h2>
+
+This is a personal website project that will provide information related to myself, projects I have completed, services I offer, and my contact information
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,20 +5,102 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Naufal Hanif</title>
+    <link rel="icon" type="image/x-icon" href="icon.png">
 </head>
 <body>
-    <div class="title-style">
-        <h1 class="title">
-            Naufal Hanif
-        </h1>
+    <div class="header">
+        <a href="#profile" class="header-button">About</a>
+        <a href="#techstack" class="header-button">Tech Stack</a>
+        <a href="" class="header-button">Projects</a>
+        <a href="" class="header-button">Services</a>
+        <a href="#contact" class="header-button">Contact</a>
     </div>
-    <div class="box-style">
-        <div class="box">
-            <h2 class="about">Profile
-                <p class="about-paragraph">
-                    Halo nama saya Naufal Hanif, saya adalah seorang mahasiswa informatika dari Universitas Syiah Kuala
-                </p>
-            </h2>
+    <div class="profile" id="profile">
+        <h1 class="hello">Hola, I'm</h1>
+        <h1 class="name">Naufal Hanif</h1>
+        <hr class="line">
+        <h1 class="status">Undergraduate Student at Syiah Kuala University</h1>
+    </div>
+    <div class="techstack" id="techstack">
+        <h1 class="ts-title">Tech Stack</h1>
+        <div class="ts-style">
+            <div class="box">
+                <image class="lang-icon" src="icon/laravel.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">Laravel</h1>
+                    <h2 class="lang-type">Back-End Framework</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/html.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">HTML</h1>
+                    <h2 class="lang-type">Front-End Framework</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/css.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">CSS</h1>
+                    <h2 class="lang-type">Front-End Framework</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/tailwindcss.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">TailwindCSS</h1>
+                    <h2 class="lang-type">Front-End Framework</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/php.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">PHP</h1>
+                    <h2 class="lang-type">Back-End Language</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/js.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">Javascript</h1>
+                    <h2 class="lang-type">Front-End Framework</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/c.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">C</h1>
+                    <h2 class="lang-type">Programming Language</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/cpp.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">C++</h1>
+                    <h2 class="lang-type">Programming Language</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/python.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">Python</h1>
+                    <h2 class="lang-type">ProgrammingLanguage</h2>
+                </div>
+            </div>
+            <div class="box">
+                <image class="lang-icon" src="icon/java.png"></image>
+                <div class="about-lang">
+                    <h1 class="lang-framework">Java</h1>
+                    <h2 class="lang-type">Programming Language</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer" id="contact">
+        <div class="footer-style">
+            <h1 class="footer-name">Naufal Hanif</h1>
+            <h1 class="footer-pipe">|</h1>
+            <h1 class="footer-copyright">Naufal Hanif © ALL RIGHTS RESERVED</h1>
         </div>
     </div>
 </body>
